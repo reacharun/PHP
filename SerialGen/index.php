4cc648a208e534d3fb668c30eb008ea5f07a200b
Arun George <mdrk07@gmail.com>
2014-04-04 10:27:21 -0700

    Create index.php
    Generate a series of random alphanumeric serial numbers.

---
+++ SerialGen/index.php
@@ -0,0 +1,49 @@
+<?php
+
+/* SerialGen - PHP
+ * Generate a series of random alphanumeric serial numbers
+ * @author Arun George, 2014
+ */
+
+// define serial attributes
+ $numberOfChars = 4;
+ $lengthOfSerial = 5;
+ 
+ $mySerialNumber = generateSerials($numberOfChars,$lengthOfSerial);
+ 
+ 
+echo "Your serial # is " . $mySerialNumber;
+
+
+
+// create a series of random serial numbers
+
+function generateSerials($numberOfChars,$lengthOfSerial) {
+
+
+$tokens = "0123456789ABCDEFGHIJKLMNOPQRSTUVWZY";
+$serial = "";
+
+for ($i = 0; $i < $lengthOfSerial; $i++) { 
+    
+    for ($j = 0; $j < $numberOfChars; $j++) { 
+        
+        $serial .= $tokens[rand(0,10)];
+        
+    }
+
+// append dash
+ if ($i < $lengthOfSerial - 1) { 
+        $serial .= "-"; 
+    }
+    
+}
+
+//let's return the value
+ return $serial;
+
+}
+//
+
+
+?>
